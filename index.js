350595ad62290a825356ba78a986cf164d6306dc
Luciana Sugeta <lulusugeta@gmail.com>
2021-09-08 14:31:14 -0400

    commit tree e0ffd273d82ca11b0369b2b9609faf87ac163490

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution here
+
+// const drivers = ["Tom", "Nick", "Gary", "Sebastian", "Gary", "Taylor", "Tom"]
+
+function findMatching(arr, str) {
+    const match = arr.filter(element => {
+        return element.toLowerCase() === str.toLowerCase()
+    });
+    return match
+}
+// findMatching(drivers, "Tom");
+
+// function fuzzyMatch(arr, query) {
+
+//     const filtered = arr.filter(function (letter) {
+//         return letter.charAt(0) === query;
+//     });
+//     return filtered
+// }
+
+function fuzzyMatch(arr, query) {
+    return arr.filter(function (el) {
+        // debugger
+        return el.toLowerCase().indexOf(query.toLowerCase()) === 0
+    })
+}
+// fuzzyMatch(drivers, "ar")
+
+function matchName(arr, str) {
+    const result = arr.filter(element => {
+        return element.name === str
+    })
+    return result
+}
